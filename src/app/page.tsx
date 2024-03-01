4c73c47e3fbef2607122d29488901b93327dcc79
leerobertdyer <leerobertdyer@gmail.com>
2024-03-01 03:47:56 -0500

    bug: fixed homepage photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
       <Header />
       <main
         style={{
-          backgroundImage: "url(/camper.jpg)",
+          backgroundImage: "url(/camper.jpeg)",
           backgroundSize: "cover",
         }}
       >
